e8be94e2c13f32fdd99ce47c1504efae1e4f2492
Radif Tajwar <radif.tajwar@pathao.com>
2026-03-05 00:40:38 +0600

    feat: Implemented database status logger and env support

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { UserModule } from './user/user.module';
 import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 import { DatabaseService } from './database/database.service';
 import { DatabaseController } from './database/database.controller';
+import { ConfigModule } from '@nestjs/config';
+import { EvService } from './ev/ev.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [DatabaseController],
-  providers: [DatabaseService],
+  providers: [DatabaseService, EvService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/database/database.controller.ts
+++ src/database/database.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { EvService } from 'src/ev/ev.service';
 
 @Controller('database')
 export class DatabaseController {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(
+    private readonly databaseService: DatabaseService,
+    private readonly evService: EvService,
+  ) {}
   @Get()
   getStatus() {
     return {
       status: this.databaseService.getStatus(),
+      DB_URL: this.evService.getDatabaseEnv(),
     };
   }
 }
